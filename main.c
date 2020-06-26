87e4f68518f4e0e914d255463876d3dad25decf6
magrini <magrinibruna@usp.br>
2020-06-26 13:47:41 -0300

    Criado menu principal

--- main.c
+++ main.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include "adjacency_list.h"
 
+#define END 0
+#define SEND_INVITE 1
+#define FRIENDSHIP_SUGGESTION 2
+#define DETECT_LOW_AFFINITY 3
+#define IDENTIFY_USER_PROFILE 4
+
+void print_menu() {
+    printf("------------------------------------\n");
+    printf("%d - Enviar convite\n", SEND_INVITE);
+    printf("%d - Sugestão de 'amigos de verdade'\n", FRIENDSHIP_SUGGESTION);
+    printf("%d - Detectar baixa afinidade\n", DETECT_LOW_AFFINITY);
+    printf("%d - Identificar perfil dos usuários\n", IDENTIFY_USER_PROFILE);
+    printf("%d - Sair\n", END);
+    printf("------------------------------------\n");
+}
+
 int main(void) {
 
     GRAPH* graph = graph_create();
@@ -18,6 +34,34 @@ int main(void) {
     graph_insert_edge(graph, "Bruna\0", "Feliz\0");
 
     graph_print(graph);
+
+    int operation;
+    print_menu();
+    scanf("%d", &operation);
+
+    while(operation != END) {
+        switch(operation) {
+            case SEND_INVITE:
+                printf("1\n");
+                break;
+            case FRIENDSHIP_SUGGESTION:
+                printf("2\n");
+                break;
+            case DETECT_LOW_AFFINITY:
+                printf("3\n");
+                break;
+            case IDENTIFY_USER_PROFILE:
+                printf("4\n");
+                break;
+            case END:
+                printf("Até a próxima!\n");
+                break;
+            default:
+                printf("Operação inválida!\n");
+        }
+        print_menu();
+        scanf("%d", &operation);
+    }
 
     graph_delete(&graph);
     
